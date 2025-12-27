17941087fd3e352e0285d2ad6adecf4ab0b00e95
Harshitpandey21 <harshitpandey1234789@gmail.com>
2025-12-28 01:33:23 +0530

    changes in chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,3 +6,15 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
+
+# Document Ingestion 
+
+video_id = "etnLX7m2MiA"
+api = YouTubeTranscriptApi()
+try:
+    transcript_list = api.fetch(video_id = video_id, languages = ["hi"])
+    transcript = " ".join(chunk.text for chunk in transcript_list)
+    print(transcript)
+
+except TranscriptsDisabled:
+    print("Transcripts are disabled for this video.")
