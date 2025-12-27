92b29f15706f8ca1b86efce7b0d1869a8214bc42
Harshitpandey21 <harshitpandey1234789@gmail.com>
2025-12-28 01:57:11 +0530

    update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,17 +4,67 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
 # Document Ingestion 
 
-video_id = "etnLX7m2MiA"
+video_id = "AUQJ9eeP-Ls"
 api = YouTubeTranscriptApi()
 try:
-    transcript_list = api.fetch(video_id = video_id, languages = ["hi"])
+    transcript_list = api.fetch(video_id = video_id, languages = ["en"])
     transcript = " ".join(chunk.text for chunk in transcript_list)
-    print(transcript)
 
 except TranscriptsDisabled:
     print("Transcripts are disabled for this video.")
+
+
+# Text Splitter 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+chunks = text_splitter.create_documents([transcript])
+
+#Embeddings & Vector Store
+embeddings = OpenAIEmbeddings(model = "text-embedding-3-small")
+vector_store = FAISS.from_documents(chunks, embeddings)
+
+#Retriever 
+
+retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+
+#Augmentation
+
+prompt = PromptTemplate(
+    template="""
+      You are a helpful assistant.
+      Answer ONLY from the provided transcript context.
+      If the context is insufficient, just say you don't know.
+
+      {context}
+      Question: {question}
+    """,
+    input_variables = ['context', 'question']
+)
+
+question          = "What is document ingestion?"
+retrieved_docs    = retriever.invoke(question)
+
+def format_docs(retrieved_docs):
+  context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+  return context_text
+
+#LLM 
+llm= ChatOpenAI(model = 'gpt-4o')
+
+#Chain 
+parallel_chain = RunnableParallel({
+    'context': retriever | RunnableLambda(format_docs),
+    'question': RunnablePassthrough()
+})
+
+parser = StrOutputParser()
+
+main_chain = parallel_chain | prompt | llm | parser
+answer = main_chain.invoke(question)
+print(answer)
